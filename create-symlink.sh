ea8a705037761d74af237926a0e528eaea274374
wudm <wudm@qcplay.com>
2015-12-10 15:31:42 +0800

    ctrlp 位置映射、ack查找选项、tab属性

---
+++ create-symlink.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Symlink the configuration files into their appropriate homes
+ln -sf ~/coding/h5/qc/vim/janus/_vimrc.before ~/.vimrc.before
+ln -sf ~/coding/h5/qc/vim/janus/_vimrc.after ~/.vimrc.after
+ln -sf ~/coding/h5/qc/vim/janus/_gvimrc.before ~/.gvimrc.before
+ln -sf ~/coding/h5/qc/vim/janus/_gvimrc.after ~/.gvimrc.after
+
