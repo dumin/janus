83bd869e716c3104eba675f79d8ea012deb979f7
wudm <wudm@qcplay.com>
2016-02-16 10:19:54 +0800

    create-symlink 使用相对路径

--- create-symlink.sh
+++ create-symlink.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Symlink the configuration files into their appropriate homes
-ln -sf ~/coding/h5/qc/vim/janus/_vimrc.before ~/.vimrc.before
-ln -sf ~/coding/h5/qc/vim/janus/_vimrc.after ~/.vimrc.after
-ln -sf ~/coding/h5/qc/vim/janus/_gvimrc.before ~/.gvimrc.before
-ln -sf ~/coding/h5/qc/vim/janus/_gvimrc.after ~/.gvimrc.after
-ln -sf ~/coding/h5/qc/vim/janus/_agignore ~/.agignore
+ln -sf ~+/_vimrc.before ~/.vimrc.before
+ln -sf ~+/_vimrc.after ~/.vimrc.after
+ln -sf ~+/_gvimrc.before ~/.gvimrc.before
+ln -sf ~+/_gvimrc.after ~/.gvimrc.after
+ln -sf ~+/_agignore ~/.agignore
